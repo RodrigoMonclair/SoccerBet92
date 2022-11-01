1df387a9b9112edd8c1036b723022674f999d97d
Rodrigo Monclair <rmonclair@gmail.com>
2022-10-31 22:48:45 -0300

    alteração das duas primeiras telas e js

--- js/MyClasses.js
+++ js/MyClasses.js
@@ -1,9 +1,13 @@
 
 class Player{
 
-    constructor(name, team){
+    constructor(name){
     this.name = name;
     this.creditos = 20;
+    this.player1Score = [];
+    this.player2Score =[];
+    this.player1SelectedTeam = "";
+    this.player2SelectedTeam = "";
 }
 
 
@@ -15,6 +19,26 @@ earnCredits(){
 
 }
 
+
+getScorePlayer1(score1,score2){
+    
+    this.player1Score.push(score1)
+    this.player1Score.push(score2)
+}
+
+
+getScorePlayer2(score1,score2){
+    this.player2Score.push(score1)
+    this.player2Score.push(score2)
+}
+
+getSelectedTeam1(team){
+    this.player1SelectedTeam = team
+}
+
+getSelectedTeam2(team){
+    this.player2SelectedTeam = team
+}
 
 
 
--- js/index.js
+++ js/index.js
@@ -7,7 +7,17 @@ const credits1 = document.getElementById("credits1")
 const credits2 = document.getElementById("credits2")
 const name1 = document.getElementById("name-player-one")
 const name2 = document.getElementById("name-player-two")
-const name = document.getElementById("name-player")
+const btnReady = document.getElementById("btn-go1")
+const btnReady2 = document.getElementById("btn-go2")
+const btnTeam1 = document.querySelectorAll(".btn-team1")
+const btnTeam2 = document.querySelectorAll(".btn-team2")
+
+
+let betAmount1 = 0;
+let betAmount2 = 0;
+
+const player1 = new Player()
+const player2 = new Player()
 
 
 btnEnter.addEventListener('click', ()=>{
@@ -18,13 +28,11 @@ btnEnter.addEventListener('click', ()=>{
 })
 
 
+
 function createPlayer(event){
-   let namePlayer1 = document.getElementById("name-player1").value
-   let namePlayer2 = document.getElementById("name-player2").value
-
-   const player1 = new Player(namePlayer1)
-   const player2 = new Player(namePlayer2)
-
+   
+   player1.name = document.getElementById("name-player1").value
+   player2.name = document.getElementById("name-player2").value
    credits1.innerText = player1.creditos
    credits2.innerText = player2.creditos
 
@@ -35,6 +43,47 @@ function createPlayer(event){
    console.log(player2) 
 }
 
+btnTeam1.forEach(btn => btn.addEventListener("click",()=> {
+    console.log(btn.value)
+    player1.getSelectedTeam1(btn.value)
+    console.log(player1)
+}))//{btn.addEventListener('click',player1.getSelectedTeam1)})
 
-const scorePlayer1 = [];
-const scorePlayer2 = [];
+
+btnTeam2.forEach(btn2 => btn2.addEventListener("click", ()=> {
+    console.log(btn2.value)
+    player2.getSelectedTeam2(btn2.value)
+    console.log(player2)
+
+
+}))//{btn.addEventListener("click", player2.getSelectedTeam2)})
+
+
+btnReady.addEventListener('click', ()=>{
+
+    
+    const score1 = document.getElementById("score-num1").value
+    const score2 =  document.getElementById("score-num2").value
+    betAmount1 = document.getElementById("bet-amount1").value
+
+    player1.getScorePlayer1(score1, score2)
+    btnReady2.disabled = false
+
+
+    console.log(player1)
+    console.log(betAmount1)
+})
+
+btnReady2.addEventListener("click", ()=>{
+    const score1 = document.getElementById("score-num3").value
+    const score2 = document.getElementById("score-num4").value
+    betAmount2 = document.getElementById("bet-amount2").value
+    
+
+    player2.getScorePlayer2(score1,score2)
+    console.log(player2)
+    console.log(betAmount2)
+
+    gameScreen.className = "hide"
+})
+
